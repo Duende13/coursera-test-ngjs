816b538fb0d01baca95655a405b75eba8f54f1e0
Susana Ruiz <susana.r.cunado@gmail.com>
2017-03-17 21:37:25 +0100

    New files and part of the solution

---
+++ module1-solution/app.js
@@ -0,0 +1,15 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+function LunchCheckController ($scope, $inject) {
+  function CheckListOfFood(){
+
+  }
+
+}
+
+
+})();

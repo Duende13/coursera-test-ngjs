66cdb91c3d784d29c945ba44c3d8c4b7164626db
Susana Ruiz <susana.r.cunado@gmail.com>
2017-03-18 11:13:34 +0100

    Basic Solution completed

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -4,12 +4,33 @@
 angular.module('LunchCheck', [])
 .controller('LunchCheckController', LunchCheckController);
 
-function LunchCheckController ($scope, $inject) {
-  function CheckListOfFood(){
+LunchCheckController.$inject = ['$scope','$filter'];
 
-  }
+function LunchCheckController($scope, $filter) {
+  $scope.checkListOfFood = function () {
+    var message = calculateMenu($scope.lunchMenu);
+    $scope.message = message;
+  };
 
-}
+
+  function calculateMenu(string) {
+    var message = 0;
+    if (string.length == 0){
+      message = "Put some food in your menu list, please.";
+    } else {
+      var itemsMenu = string.split(",");
+      if (itemsMenu.length == 0){
+      } if (itemsMenu.length > 0 && itemsMenu.length < 4) {
+        message = "Enjoy!!!";
+      } else {
+        message = "Too Much!!!";
+      }
+    }
+    return message;
+  };
+
+
+};
 
 
 })();
